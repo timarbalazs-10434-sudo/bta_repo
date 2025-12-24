03b520574f731b0eb1e8ceec73b9943e1b7fe28c
timarbalazs32 <234422759+timarbalazs-10434-sudo@users.noreply.github.com>
2025-12-24 17:36:32 +0100

    Frontend+Backend egy mappában
    Mivel a frontend (web) kapcsolatban van a backenddel (Node.js, adatbázis) ezért egy mappába raktam őket

--- frontend/btaWeb/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Profile } from './profile/profile';
-
-export const routes: Routes = [
-  // Fontos: itt ne legyen per jel az út elején!
-  { path: 'profile', component: Profile }
-];
