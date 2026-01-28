62a70e10aa85dbd6175247230c34591b47ef7624
Szabi <furakszabolcsrobert-10329@taszi.hu>
2026-01-28 20:58:51 +0100

    Clean Angular project+Frontend alapjainak része+Routing+Angular elrendezés+minimális design

---
+++ Frontend/BeneathTheAshes/src/app/download/download.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download',
+  imports: [],
+  templateUrl: './download.html',
+  styleUrl: './download.css',
+})
+export class Download {
+
+}
